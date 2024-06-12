c845af316e91d841b1b42d4e9a5d4b25711ef755
SaeMiPark <samming3013@gmail.com>
2024-06-12 18:16:26 +0900

    create QBoardDTO, QCategoryDTO, QReplyDTO.java

--- src/main/java/dto/QBoardDTO.java
+++ src/main/java/dto/QBoardDTO.java
@@ -5,15 +5,108 @@
  */
 package dto;
 
+import java.sql.Timestamp;
+
 /**
  * Description : 클래스에 대한 설명을 입력해주세요.
  * Date : 2024. 6. 12.
  * History :
- *  - 작성자 : Jin, 날짜 : 2024. 6. 12., 설명 : 최초작성
+ *  - 작성자 : Sam, 날짜 : 2024. 6. 12., 설명 : 최초작성
  *
- * @author : Jin 
+ * @author : Sam 
  * @version 1.0 
  */
 public class QBoardDTO {
+	private int qBoardSeq;
+	private String userId;
+	private int qBoardCategory;
+	private String qBoardTitle;
+	private String qBoardContent;
+	private Timestamp qBoardDate;
+	private String qBoardAnswer;
+	private String qBoardSecret;
+	
+	public QBoardDTO() {}
+
+	public QBoardDTO(int qBoardSeq, String userId, int qBoardCategory, String qBoardTitle, String qBoardContent,
+			Timestamp qBoardDate, String qBoardAnswer, String qBoardSecret) {
+		this.qBoardSeq = qBoardSeq;
+		this.userId = userId;
+		this.qBoardCategory = qBoardCategory;
+		this.qBoardTitle = qBoardTitle;
+		this.qBoardContent = qBoardContent;
+		this.qBoardDate = qBoardDate;
+		this.qBoardAnswer = qBoardAnswer;
+		this.qBoardSecret = qBoardSecret;
+	}
+
+	public int getqBoardSeq() {
+		return qBoardSeq;
+	}
+
+	public void setqBoardSeq(int qBoardSeq) {
+		this.qBoardSeq = qBoardSeq;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getqBoardCategory() {
+		return qBoardCategory;
+	}
+
+	public void setqBoardCategory(int qBoardCategory) {
+		this.qBoardCategory = qBoardCategory;
+	}
+
+	public String getqBoardTitle() {
+		return qBoardTitle;
+	}
+
+	public void setqBoardTitle(String qBoardTitle) {
+		this.qBoardTitle = qBoardTitle;
+	}
+
+	public String getqBoardContent() {
+		return qBoardContent;
+	}
+
+	public void setqBoardContent(String qBoardContent) {
+		this.qBoardContent = qBoardContent;
+	}
+
+	public Timestamp getqBoardDate() {
+		return qBoardDate;
+	}
+
+	public void setqBoardDate(Timestamp qBoardDate) {
+		this.qBoardDate = qBoardDate;
+	}
+
+	public String getqBoardAnswer() {
+		return qBoardAnswer;
+	}
+
+	public void setqBoardAnswer(String qBoardAnswer) {
+		this.qBoardAnswer = qBoardAnswer;
+	}
+
+	public String getqBoardSecret() {
+		return qBoardSecret;
+	}
+
+	public void setqBoardSecret(String qBoardSecret) {
+		this.qBoardSecret = qBoardSecret;
+	}
+	
+	
+	
+	
+	
 
 }
--- src/main/java/dto/QReplyDTO.java
+++ src/main/java/dto/QReplyDTO.java
@@ -5,15 +5,63 @@
  */
 package dto;
 
+import java.sql.Timestamp;
+
 /**
  * Description : 클래스에 대한 설명을 입력해주세요.
  * Date : 2024. 6. 12.
  * History :
- *  - 작성자 : Jin, 날짜 : 2024. 6. 12., 설명 : 최초작성
+ *  - 작성자 : Sam, 날짜 : 2024. 6. 12., 설명 : 최초작성
  *
- * @author : Jin 
+ * @author : Sam 
  * @version 1.0 
  */
 public class QReplyDTO {
+	private int qReplySeq;
+	private int qBoardSeq;
+	private String userId;
+	private String qReplyContent;
+	private Timestamp qReplyDate;
+	
+	public QReplyDTO() {}
+	
+	public QReplyDTO(int qReplySeq, int qBoardSeq, String userId, String qReplyContent, Timestamp qReplyDate) {
+		this.qReplySeq = qReplySeq;
+		this.qBoardSeq = qBoardSeq;
+		this.userId = userId;
+		this.qReplyContent = qReplyContent;
+		this.qReplyDate = qReplyDate;
+	}
+	public int getqReplySeq() {
+		return qReplySeq;
+	}
+	public void setqReplySeq(int qReplySeq) {
+		this.qReplySeq = qReplySeq;
+	}
+	public int getqBoardSeq() {
+		return qBoardSeq;
+	}
+	public void setqBoardSeq(int qBoardSeq) {
+		this.qBoardSeq = qBoardSeq;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getqReplyContent() {
+		return qReplyContent;
+	}
+	public void setqReplyContent(String qReplyContent) {
+		this.qReplyContent = qReplyContent;
+	}
+	public Timestamp getqReplyDate() {
+		return qReplyDate;
+	}
+	public void setqReplyDate(Timestamp qReplyDate) {
+		this.qReplyDate = qReplyDate;
+	}
+
 
 }
