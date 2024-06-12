19efe6fe5d9cc0bb9a53cff09e1f82ee058a88ed
Jin <sebgokti4276@naver.com>
2024-06-12 16:46:05 +0900

    Project Initialization

---
+++ src/main/java/dto/ReviewDTO.java
@@ -0,0 +1,19 @@
+/**
+ * 팀 별빛, Software License, Version 1.0
+ *
+ * Copyright (c) 팀 별빛, All rights reserved.
+ */
+package dto;
+
+/**
+ * Description : 클래스에 대한 설명을 입력해주세요.
+ * Date : 2024. 6. 12.
+ * History :
+ *  - 작성자 : Jin, 날짜 : 2024. 6. 12., 설명 : 최초작성
+ *
+ * @author : Jin 
+ * @version 1.0 
+ */
+public class ReviewDTO {
+
+}
