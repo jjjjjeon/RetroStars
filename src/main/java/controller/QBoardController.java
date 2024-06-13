ebd2dc1ab6237c71de6340ec8b8f685c98b755fa
SaeMiPark <samming3013@gmail.com>
2024-06-13 17:18:58 +0900

    feat qboard/mainBoard.jsp select

--- src/main/java/controller/QBoardController.java
+++ src/main/java/controller/QBoardController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
+import common.Static;
 import dao.QBoardDAO;
 import dto.QBoardDTO;
 
@@ -17,7 +20,7 @@ import dto.QBoardDTO;
  * Description : 클래스에 대한 설명을 입력해주세요.
  * Date : 2024. 6. 12.
  * History :
- *  - 작성자 : Sam, 날짜 : 2024. 6. 12., 설명 : 최초작성
+ *  - 작성자 : Sam, 날짜 : 2024. 6. 13., 설명 : 최초작성
  *
  * @author : Sam
  * @version 1.0 
@@ -43,6 +46,23 @@ public class QBoardController extends HttpServlet {
 				String qBoardContent= request.getParameter("qBoardContent");
 				String qBoardSecret=boarddao.getSecretYN(request.getParameter("qBoardSecret")) ;
 				boarddao.insert(new QBoardDTO(0,userId,qBoardCategory,qBoardTitle,qBoardContent,null,"N",qBoardSecret));
+				
+			}else if(cmd.equals("/select.qboard")) {
+				String pcpage=request.getParameter("cpage");
+				if(pcpage==null) {pcpage="1";}
+				int cpage=Integer.parseInt(pcpage);
+				System.out.println(cpage);
+				int recordCountPerPage = Static.QBOARD_RECOD_COUNT_PER_PAGE;
+	            int naviCountPerPage = Static.QBOARD_NAVI_COUNT_PER_PAGE;
+	            int recordTotalCount = boarddao.getRecordCount();
+				ArrayList<QBoardDTO> list=
+						boarddao.select(
+								cpage*Static.QBOARD_RECOD_COUNT_PER_PAGE-(Static.QBOARD_RECOD_COUNT_PER_PAGE-1),
+								cpage*Static.QBOARD_RECOD_COUNT_PER_PAGE);
+				
+				request.setAttribute("list", list);
+				String json = g.toJson(new Object[] { cpage, recordCountPerPage, naviCountPerPage, recordTotalCount, list });
+		        response.getWriter().write(json);
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
